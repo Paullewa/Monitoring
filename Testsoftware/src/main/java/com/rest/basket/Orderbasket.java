12b9aedbc0ebec222837dc574181392455e8bff0
mwithb2019 <mwithb2019@gmail.com>
2019-12-29 09:46:03 +0100

    new classe was created

--- Testsoftware/src/main/java/com/rest/basket/Orderbasket.java
+++ Testsoftware/src/main/java/com/rest/basket/Orderbasket.java
@@ -1,11 +1,64 @@
 package com.rest.basket;
 
+import com.rest.order_product.Order;
+import com.sun.xml.txw2.annotation.XmlElement;
+
 public class Orderbasket {
 
-  protected  int id_logger;
-  protected Boolean Is_connected;
-  protected  Boolean Is_Disconnected;   
-  //test
+  protected  Order Orderbasket;
+  //Id for the item inside our badket
+  protected int Item_id;
+  // Informe us about the situation our Basket
+  public String Status;
+public Orderbasket() {
+	
+	Orderbasket = null;
+	Item_id = -1;
+	Status = " ";
+}
+public Orderbasket(Order _orderbasket, int _item_id, String _status) {
+	
+	Orderbasket = _orderbasket;
+	Item_id = _item_id;
+	Status = _status;
+	
+}
+//need to collect all index
+@XmlElement
+public Order getOrderbasket(Order Orderb) {
+	 {
+		for(var index =0; index< Orderb.getId().indexOf(Item_id); ++index )
+		{
+			 ;
+		}
+	}
+	return Orderbasket;
+}
+
+public void setOrderbasket(Order orderbasket) {
+	Orderbasket = orderbasket;
+}
+// collect all Item_id an set it inside a array
+@XmlElement
+public int getItem_id(int items[]) {
+	 for(int i= 0; i<= items.length;i++) {
+		Item_id = items[i]; 
+	 }
+	return Item_id;
+	
+}
+
+public void setItem_id(int item_id) {
+	Item_id = item_id;
+}
+
+public String getStatus() {
+	return Status;
+}
+public void setStatus(String status) {
+	Status = status;
+}   
   
 
+  
 }
--- Testsoftware/src/main/java/com/rest/basket/OrderbasketService.java
+++ Testsoftware/src/main/java/com/rest/basket/OrderbasketService.java
@@ -1,5 +1,77 @@
 package com.rest.basket;
 
+import java.util.Date;
+import java.util.HashMap;
+
+import java.util.Map;
+import java.util.UUID;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import com.rest.order_product.Order;
+
+
+@Path("/basket")
 public class OrderbasketService {
+	
+private Map<String,Orderbasket>Bask ;
+
+public OrderbasketService(){	
+   String testkey = UUID.randomUUID().toString();
+   Bask =  new HashMap<String, Orderbasket>();
+
+   
+   Orderbasket desor = new Orderbasket();
+  //Orderbasket.getId acced the Order_id our class order_product
+   desor.setOrderbasket(new Order());
+   desor.setStatus("new Items Added");
+   desor.setItem_id(0);
+   //desor.Orderbasket.setDateCreated((java.sql.Date) new Date());
+   
+    Bask.put(testkey, desor);
 
 }
+
+@GET
+@Consumes
+ public String getbasket() {	 
+	 Gson gson = new Gson();
+	 return gson.toJson( Bask.values().toArray(new Orderbasket[Bask.size()]));
+	
+}
+
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/id")
+public String getOdreBaskettoJSON(@PathParam("id") String id)
+{
+    Gson gson = new Gson();
+    	String result = gson.toJson(Bask);
+    			
+	return result;
+}
+@POST
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_HTML)	
+ public Response getrOrderB(String O_OrderBask)
+ {
+	Gson gson = new Gson();
+	Orderbasket response = gson.fromJson(O_OrderBask,Orderbasket.class);
+		response.setItem_id(0);
+	    response.setStatus("new item  was added");
+	    response.setOrderbasket(new Order()); 
+	    String testkey = UUID.randomUUID().toString();
+	    
+	 Bask.put(testkey, response);
+	 return Response.status(200).build();
+	 
+ }
+}
+	
